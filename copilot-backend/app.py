e3a4d1e13790bea5d6a4f112891d6c15e8890dca
mujahidniaz <mniaz@stratusgreen.com>
2024-08-09 04:16:53 +0200

    initial commit

---
+++ copilot-backend/app.py
@@ -0,0 +1,31 @@
+import os
+
+from flask import Flask, request
+from flask_socketio import SocketIO, emit
+from flask_cors import CORS
+import ollama
+
+app = Flask(__name__)
+CORS(app)
+socketio = SocketIO(app, cors_allowed_origins="*")
+ollama_host = os.getenv('OLLAMA_HOST', 'localhost')
+ollama_port = os.getenv('OLLAMA_PORT', 11434)
+print("Pulling Llama-3.1")
+ollama.pull("llama3.1")
+
+
+@socketio.on('send_message')
+def handle_message(data):
+    message = data.get('message', '')
+
+    try:
+        stream = ollama.chat(model='llama3.1', messages=[{'role': 'user', 'content': message}], stream=True)
+        for chunk in stream:
+            emit('receive_message', {'content': chunk['message']['content']})
+    except Exception as e:
+        print(f"Error: {e}")
+        emit('receive_message', {'content': "Error generating response"})
+
+
+if __name__ == '__main__':
+    socketio.run(app, host='0.0.0.0', port=8000, debug=True,allow_unsafe_werkzeug=True)
